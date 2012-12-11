9499cb376d3822763e94819baffbae0c7869476b
Hilary Cheng <hilarycheng@gmail.com>
2012-12-11 23:52:02 +0800

    - Add Host Software to Read res/raw/quick and generate a quick table to   C language. It should be much faster - Update Usage Frequency at Native Side - Get the Match from Native, Native Side will sort the character   according to frequency

--- src/com/diycircuits/cangjie/TableLoader.java
+++ src/com/diycircuits/cangjie/TableLoader.java
@@ -4,7 +4,10 @@ public class TableLoader {
 
     public native char getChar();
     public native char passCharArray(char[] array);
-    public native int updateFrequencyQuick(char[][] array, char ch);
+    public native int  updateFrequencyQuick(char ch);
+    public native void searchQuick(char index0, char index1);
+    public native int totalMatch();
+    public native char getMatchChar(int index);
 
     static {
 	System.loadLibrary("chinese_table");
