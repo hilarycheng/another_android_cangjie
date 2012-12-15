a3f39ed40c732b75f0a34ff9a4bcb1e211cc54f3
Hilary Cheng <hilarycheng@gmail.com>
2012-12-15 20:14:51 +0800

    - Change to Use Quick Classics from ibus-tables

--- host/java/Convert.java
+++ host/java/Convert.java
@@ -1,18 +1,24 @@
 import java.awt.*;
 import java.io.*;
+import java.util.*;
 
 public class Convert {
 
     public static void convertQuick() {
 	try {
+
+	    HashMap<String, ArrayList<Character>> charList = 
+		new HashMap<String, ArrayList<Character>>();
+	    
 	    Font font = new Font("Droid Sans Fallback", 16, Font.PLAIN);
 	    int totalQuickColumn = 3;
-	    FileInputStream fis = new FileInputStream("quick3.txt");
+	    FileInputStream fis = new FileInputStream("quick-classic.txt");
 	    InputStreamReader input = new InputStreamReader(fis, "UTF-8");
 	    BufferedReader reader = new BufferedReader(input);
 	    String str = null;
 	    int index = 0;
 	    int total = 0;
+	    ArrayList<String> keyList = new ArrayList<String>();
 	    System.out.println("#define QUICK_COLUMN " + totalQuickColumn);
 	    System.out.println("const jchar quick[][QUICK_COLUMN] = {");
 	    do {
@@ -22,26 +28,45 @@ public class Convert {
 		index = str.indexOf('\t');
 		if (index < 0) index = str.indexOf(' ');
 		if (index > 0) {
-		    int type = Character.getType(str.charAt(index + 1));
-		    // if (Character.isLetter(str.charAt(index + 1)) ||
-		    // 	type == Character.START_PUNCTUATION || type == Character.END_PUNCTUATION ||
-		    // 	type == Character.OTHER_PUNCTUATION || type == Character.MATH_SYMBOL ||
-		    // 	type == Character.DASH_PUNCTUATION  || type == Character.CONNECTOR_PUNCTUATION ||
-		    // 	type == Character.OTHER_SYMBOL      || type == Character.INITIAL_QUOTE_PUNCTUATION ||
-		    // 	type == Character.FINAL_QUOTE_PUNCTUATION || type == Character.SPACE_SEPARATOR) {
-			if (font.canDisplay(str.charAt(index + 1))) {
-			    System.out.print("\t { ");
-			    if ((int) str.charAt(1) == 9 || str.charAt(1) == ' ')  {
-				System.out.print("'" + str.charAt(0) + "',   0, ");
-			    } else {
-				System.out.print("'" + str.charAt(0) + "', '" + str.charAt(1) + "', ");
-			    }
-			    System.out.println((int) str.charAt(index + 1) + " }, ");
-			    total++;
+		    if (font.canDisplay(str.charAt(index + 1))) {
+			StringBuffer sb = new StringBuffer();
+			// System.out.print("\t { ");
+			if ((int) str.charAt(1) == 9 || str.charAt(1) == ' ')  {
+			    // System.out.print("'" + str.charAt(0) + "',   0, ");
+			    sb.append(str.charAt(0));
+			} else {
+			    sb.append(str.charAt(0));
+			    sb.append(str.charAt(1));
+			    // System.out.print("'" + str.charAt(0) + "', '" + str.charAt(1) + "', ");
 			}
-		    // }
+			String key = sb.toString();
+			// System.out.println((int) str.charAt(index + 1) + " }, ");
+			Character ch = new Character(str.charAt(index + 1));
+
+			if (!keyList.contains(key)) keyList.add(key);
+			
+			if (charList.containsKey(key)) {
+			    charList.get(key).add(ch);
+			} else {
+			    ArrayList<Character> c = new ArrayList<Character>();
+			    c.add(ch);
+			    charList.put(key, c);
+			}
+			total++;
+		    }
 		}
 	    } while (str != null);
+	    for (int count = 0; count < keyList.size(); count++) {
+		String k = keyList.get(count);
+		ArrayList<Character> l = charList.get(k);
+		for (int loop = 0; loop < l.size(); loop++) {
+		    if (k.length() == 1) {
+			System.out.println("\t { '" + k.charAt(0) + "',   0, " + l.get(loop) + " }, ");
+		    } else {
+			System.out.println("\t { '" + k.charAt(0) + "', '" + k.charAt(1) + "', " + l.get(loop) + " }, ");
+		    }
+		}
+	    }
 	    System.out.println("};");
 	    System.out.println("jint quick_index[" + total + "];");
 	    System.out.println("jint quick_frequency[" + total + "];");
